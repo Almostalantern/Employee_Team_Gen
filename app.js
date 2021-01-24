29b98370aa8ae5b387efe02f259ec1916b0975dc
John Ciancone <jackccinc@gmail.com>
2021-01-24 08:15:39 -0500

    Now with working interns and engineers

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ function switchStatement() {
                     addEngineer()
                     break;
                 default:
-              createTeam()
+                    createTeam()
             }
         })
 }
@@ -41,11 +41,11 @@ const manQ = [
         type: "input",
         message: "What is the manager's name?",
         name: "managerName",
-        validate: answer =>{
-            if (answer!== ""){
+        validate: answer => {
+            if (answer !== "") {
                 return true;
             }
-            return "Please enter name";
+            return "Please enter a name";
         }
     },
     {
@@ -64,6 +64,66 @@ const manQ = [
         name: "officeNum"
     }
 ]
+
+const intQ = [
+    {
+        type: "input",
+        message: "What is the intern's name?",
+        name: "intName",
+        validate: answer => {
+            if (answer !== "") {
+                return true;
+            }
+            return "Please enter a name";
+        }
+    },
+    {
+        type: "input",
+        message: "What is the intern's ID number",
+        name: "intID"
+    },
+    {
+        type: "input",
+        message: "What is the intern's email address?",
+        name: "intEmail"
+    },
+    {
+        type: "input",
+        message: "What is the intern's school?",
+        name: "intSchool"
+    }
+]
+
+const enginQ = [
+    {
+        type: "input",
+        message: "What is the engineer's name?",
+        name: "enginName",
+        validate: answer => {
+            if (answer !== "") {
+                return true;
+            }
+            return "Please enter a name.";
+        }
+    },
+    {
+        type: "input",
+        message:"What is the engineer's ID number?",
+        name:"enginNumber",
+    },
+    {
+        type:"input",
+        message:"What is the Engineer's Email Address?",
+        name:"enginEmail",
+    },
+    {
+        type:"input",
+        message:"What is the engineer's github account name?",
+        name:"enginGithub"
+    }
+
+]
+
 function addManager() {
     inquirer.prompt(manQ).then(
         function (response) {
@@ -72,33 +132,31 @@ function addManager() {
             switchStatement()
         }
     );
-
 }
-function createTeam(){
-    if (!fs.existsSync(OUTPUT_DIR)){
+function addIntern() {
+    inquirer.prompt(intQ).then(
+        function (response) {
+            const intern = new Intern(response.intName, response.intID, response.intEmail, response.intSchool)
+            team.push(intern)
+            switchStatement()
+        }
+    )
+}
+function addEngineer(){
+    inquirer.prompt(enginQ).then(
+        function (response) {
+            const engineer = new Engineer(response.enginName, response.enginNumber, response.enginEmail, response.enginGithub )
+        team.push(engineer)
+        switchStatement()
+        }
+    )
+}
+
+
+function createTeam() {
+    if (!fs.existsSync(OUTPUT_DIR)) {
         fs.mkdirSync(OUTPUT_DIR)
-    }fs.writeFileSync(outputPath, render(team), "utf-8")
+    } fs.writeFileSync(outputPath, render(team), "utf-8")
 }
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
 switchStatement()
